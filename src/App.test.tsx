fe16051fcd4ca6630bb9ea1b82d47d54c18c6212
maxvektor <m.kocijevsky@gmail.com>
2023-06-05 15:20:43 +0200

    test: fix test and add tests on build

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByText(/STAR WARS MOVIES/i);
+  expect(header).toBeInTheDocument();
 });
