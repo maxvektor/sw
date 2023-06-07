9f018c8db7c09eb1b79ba273100976cbc467c66b
maxvektor <m.kocijevsky@gmail.com>
2023-06-07 23:24:12 +0200

    chore: take movie from cache if possible

--- packages/client/src/hooks/data.ts
+++ packages/client/src/hooks/data.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getMovies, getMovie } from "../api";
 import type { IMovie } from "../types";
 
@@ -19,7 +19,22 @@ export const useMovies:IDataCollection<IMovie> = () => {
 };
 
 export const useMovie:IDataItem<IMovie> = (id: number) => {
-  const query = useQuery({ queryKey: ["movies", id.toString()], queryFn: () => getMovie(id) });
+  const queryClient = useQueryClient();
+
+  const queryFn = async (id: number) =>{
+    debugger;
+    
+    const movies = queryClient.getQueryData<IMovie[]>(["movies"]);
+    // one more check for the case when we have a movie in the cache
+    if(movies){
+      const movie = movies.find((movie) => movie.episode_id === id);
+      if(movie) return movie;
+    }
+
+    return getMovie(id)
+  }
+
+  const query = useQuery({ queryKey: ["movies", id.toString()], queryFn: () => queryFn(id)});
   const { data, isLoading, isError } = query;
   
   return { data, isLoading, isError };
