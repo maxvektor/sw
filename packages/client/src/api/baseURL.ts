d9d518770721331f3f6a6a25ea89e5a4dc10ffe7
maxvektor <m.kocijevsky@gmail.com>
2023-06-08 00:14:57 +0200

    chore: small fix to make vercel run

--- packages/client/src/api/baseURL.ts
+++ packages/client/src/api/baseURL.ts
@@ -1,11 +1,11 @@
 export const getServetBaseUrl = () => {
-  const PR_ID = process.env.VERCEL_GIT_PULL_REQUEST_ID;
+  const PR_ID = process.env.REACT_APP_VERCEL_GIT_PULL_REQUEST_ID;
   
   if(PR_ID){
     return `https://maxvektor-starwars-server-pr-${PR_ID}.onrender.com`
   }
 
-  if(process.env.NODE_ENV === 'production'){
+  if(process.env.REACT_APP_VERCEL_ENV === 'production'){
     return `https://maxvektor-starwars-server.onrender.com`
   }
 
