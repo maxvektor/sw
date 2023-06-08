ae9b6c24289b91f93c2f398eeeade127ff65a675
maxvektor <m.kocijevsky@gmail.com>
2023-06-08 18:41:18 +0200

    feat: movie page

--- packages/client/src/components/Movie/Movie.tsx
+++ packages/client/src/components/Movie/Movie.tsx
@@ -9,13 +9,36 @@ export const Movie: React.FC<{ id: number }> = ({ id }) => {
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Somethig went wrong...</div>;
 
-  const { title } = data!;
+  const {
+    title,
+    opening_crawl,
+    director = "unknown",
+    producer = "unknown",
+    created,
+  } = data!;
+  const creationDate = new Date(created);
+  const year = creationDate.getFullYear();
+  const monts = creationDate.getUTCMonth();
 
   return (
-    <div className={styles.root}>
+    <article className={styles.root}>
       <h1 className={styles.header} data-testid={"movie-title"}>
         Episode {id}: {title}
       </h1>
-    </div>
+
+      <div className={styles.created}>
+        created: <time dateTime={`${year}-${monts}`}>{`${monts}/${year}`}</time>
+      </div>
+      <div className={styles.description}>
+        <p>{opening_crawl}</p>
+
+        <dl>
+          <dt className={styles.definition_term}>director</dt>
+          <dd className={styles.definition}>{director}</dd>
+          <dt className={styles.definition_term}>producer</dt>
+          <dd className={styles.definition}>{producer}</dd>
+        </dl>
+      </div>
+    </article>
   );
 };
